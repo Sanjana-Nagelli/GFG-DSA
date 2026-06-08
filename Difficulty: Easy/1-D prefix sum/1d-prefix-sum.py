e66392b3761cf538d5114f2e1f5cc1ec1050fd6b
Sanjana Nagelli <67863203+Sanjana-Nagelli@users.noreply.github.com>
2026-06-08 14:32:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/1-D prefix sum/1d-prefix-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def prefSum(self, arr):
+        prefix = [0] * len(arr)
+        
+        prefix[0] = arr[0]
+
+        for i in range(1, len(arr)):
+            prefix[i] = prefix[i - 1] + arr[i]
+
+        return prefix
+        
